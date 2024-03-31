2ea530bbc1ad6873d060adf88f58a61566a106c5
Judah Lit <Judahlit03@gmail.com>
2024-04-01 00:14:17 +0200

    Created Spring Boot app with Docker files

---
+++ src/test/java/com/wavshare/EmailService/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wavshare.EmailService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
